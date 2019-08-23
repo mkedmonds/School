ce44e6c6fd7f13392d83230d661865e05b81ae36
madeline edmonds <madness981@live.com>
2019-08-23 12:09:30 -0500

    Add existing file

---
+++ testGit.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+  
+    <title>WDV341: GIT TEST</title>
+
+</head>
+<body>
+
+    <?php echo "<h3>Madeline Edmonds</h3>" ; ?>
+
+    <h1>WDV341: Git Terms and Test Page</h1>
+
+    <ol>
+
+        <li><strong>Version Control Software</strong> : A way to keep tract of the versions of you code everytime you make edits.</li>
+        <li><strong>Add</strong> : adds file to be commited to the responsitory</li>
+        <li><strong>Commit</strong> : saves file copy to local respository(cmd line)</li>
+        <li><strong>Push</strong> : pushes file from local respository to remote respository(going from folder to GitHub)</li>
+        <li><strong>Pull</strong> : downloads file from GitHub to your local respository</li>
+        <li><strong>Clone</strong> : makes a duplicate responsitory in a new place.</li>
+
+    </ol>
+
+</body>
+</html>
