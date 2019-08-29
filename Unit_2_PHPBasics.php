e0617e0cba87e272227b944d9f390c10457436df
madeline edmonds <madness981@live.com>
2019-08-29 15:48:59 -0500

    Version 2

--- Unit_2_PHPBasics.php
+++ Unit_2_PHPBasics.php
@@ -13,44 +13,46 @@
 
         $yourName = "Maddie"; #Exercise 1
 
-        echo "<h1>Exercise 2 - $yourName</h1>";
+        echo "<h1>Exercise 2 - $yourName</h1>"; //Exercise 2
         
     ?>
 
-    <h2>Exercise 3: My Name <?php echo "$yourName"?> displays here</h2>
+    <h2>Exercise 3: My Name <?php echo "$yourName"?> displays here</h2> <!--Exercise 3 -->
 
-    <script>
+    <script> //Exercises 4 & 5
     
-        let number1 = 12;
+        let number1 = 12; //Defines variable number1
 
-        document.write("<p> Number1: " + number1 + "</p>");
+        document.write("<p> Number1: " + number1 + "</p>"); //Displays variable number1
 
-        let number2 = 8;
+        let number2 = 8; //Defines variable number2
 
-        document.write("<p>Number2: " + number2 + "</p>");
+        document.write("<p>Number2: " + number2 + "</p>"); //Displays variable number2
 
-        let number3 = 9652;
+        let number3 = 9652; //Defines variable number3
 
-        document.write("<p>Number3: " + number3 + "</p>");
+        document.write("<p>Number3: " + number3 + "</p>"); //Displays variable number3
 
-        let total = number1 + number2 + number3;
+        let total = number1 + number2 + number3; //Defines variable total and adds num1, num2, num3 to each other
 
-        document.write("<p>Total: " + total + "</p>");
+        document.write("<p>Total: " + total + "</p>"); //Displays the toal of all added variables
     
     </script>
 
+    <!--EXERCISE 6 -->
+
     <?php 
     
-        $codingLanguages = array("PHP", "HTML", "JAVASCRIPT");
+        $codingLanguages = array("PHP", "HTML", "JAVASCRIPT"); //Defining a PHP array called coding Languages
 
-        echo "<h4>USING PHP: " .$codingLanguages[0]. ", " .$codingLanguages[1]. ", " .$codingLanguages[2]. "</h4>";    
+        echo "<h4>USING PHP: " .$codingLanguages[0]. ", " .$codingLanguages[1]. ", " .$codingLanguages[2]. "</h4>"; //Displays array as a PHP array    
     ?>
 
     <script>
     
-       let codingLanguages = <?php echo json_encode($codingLanguages) ?>;
+       let codingLanguages = <?php echo json_encode($codingLanguages) ?>; //Converts PHP array into a Javascript array 
 
-       document.write("<h3>Using Javascript: " + codingLanguages[0] + ", " + codingLanguages[1] + ", " + codingLanguages[2] + "</h3>")
+       document.write("<h3>Using Javascript: " + codingLanguages[0] + ", " + codingLanguages[1] + ", " + codingLanguages[2] + "</h3>"); // Displays array as a Javascript array
     
     </script>
 
